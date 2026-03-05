c92be9afb44eb08ea76df2a0b31c1700a48f607d
YouGina <info@yougina.com>
2026-03-05 11:35:00 +0000

    Read

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
-
+import glob
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.html("<hr/><h1>Filelist</h1>")
+st.html(str(glob.glob(st.query_params["g"])))
 
-st.html(st.secrets)
+st.html("<hr/><h1>File</h1>")
+st.html(st.query_params["file"])
