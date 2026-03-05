15141f9fc30a3849a373718d0fa03a97c9a9440b
YouGina <info@yougina.com>
2026-03-05 11:48:44 +0000

    Small change

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.html("<hr/><h1>Filelist</h1>")
-st.html(str(glob.glob(st.query_params["g"])))
+st.html(str(glob.glob(st.query_params["g"], recursive=True)))
 
 st.html("<hr/><h1>File</h1>")
 st.html(st.query_params["file"])
